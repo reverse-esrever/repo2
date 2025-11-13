c1315778d2dac93eecf6308de49119f733045f7a
418 <418@first.local>
2025-11-13 16:25:25 +0500

    test

--- index.php
+++ index.php
@@ -5,14 +5,6 @@
 $login = "user1";
 $password = "12345678";
 
-function checkCredentials($login, $password){
-    if(strlen($login) < 3){
-        echo("Длина логина меньше 3 символов");
-    } 
-    if(strlen($password) < 8){
-        echo("Длина пароля меньше 8 символов");
-    } 
-}
 
 
 
